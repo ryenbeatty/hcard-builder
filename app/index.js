346862042dc78130479b22eca12e5f5c7f439d7a
Geoffrey Chong <gchong@deloitte.com.au>
2017-01-02 11:09:58 +0800

    Added babel polyfill to fix IE

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ import './css/base.scss';
 // Some ES6+ features require the babel polyfill
 // More info here: https://babeljs.io/docs/usage/polyfill/
 // Uncomment the following line to enable the polyfill
-// require("babel/polyfill");
+require('babel/polyfill');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
