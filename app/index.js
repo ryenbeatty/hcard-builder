f59e5469db342f6e90f34112edb30229d2ca4ef9
Geoffrey Chong <gchong@deloitte.com.au>
2017-01-02 11:17:01 +0800

    fixed babel polyfill dependency

--- app/index.js
+++ app/index.js
@@ -2,10 +2,7 @@
 // to get around CSS order randomness in webpack.
 import './css/base.scss';
 
-// Some ES6+ features require the babel polyfill
-// More info here: https://babeljs.io/docs/usage/polyfill/
-// Uncomment the following line to enable the polyfill
-require('babel/polyfill');
+import 'babel-polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
