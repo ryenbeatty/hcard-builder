c7eefa7f52e11b83d4fb961d223d892bc80e28dc
Geoffrey Chong <gchong@deloitte.com.au>
2017-01-16 13:07:48 +1100

    init

--- app/components/Form/UploadAvatar/UploadAvatar.js
+++ app/components/Form/UploadAvatar/UploadAvatar.js
@@ -6,12 +6,19 @@ import styles from './styles';
 import button from '../../../css/button';
 import utility from '../../../css/utility';
 
+import Dropzone from 'react-dropzone';
+
 const UploadAvatar = (props) => {
 	return (
-		<label htmlFor="upload" className={classnames(button.base, styles.base)}>
-			Upload Avatar
-			<input id="upload" type="file" className={utility.visuallyHidden} onChange={props.handleAvatarUpload} />
-		</label>
+		<div>
+			<Dropzone onDrop={props.handleAvatarUpload}>
+				<div>Try dropping some files here, or click to select files to upload.</div>
+			</Dropzone>
+			<label htmlFor="upload" className={classnames(button.base, styles.base)}>
+				Upload Avatar
+				<input id="upload" type="file" className={utility.visuallyHidden} onChange={props.handleAvatarUpload} />
+			</label>
+		</div>
 	);
 };
 
